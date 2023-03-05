4c1cf05c53b7c442fefb317108098fe3c44746d5
Aaron Zamanzadeh <Zaman.aaron2@gmail.com>
2023-03-05 15:55:46 -0800

    phase 0 fin

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,49 @@
 const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+
 const app = express();
-require('dotenv').config();
+
+
+
+app.use(morgan('dev'));
+
+app.use(cookieParser());
 app.use(express.json());
+// Security Middleware
+if (!isProduction) {
+    // enable cors only in development
+    app.use(cors());
+  }
+
+  // helmet helps set a variety of headers to better secure your app
+  app.use(
+    helmet.crossOriginResourcePolicy({
+      policy: "cross-origin"
+    })
+  );
+
+  // Set the _csrf token and create req.csrfToken method
+  app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+      }
+    })
+  );
+  const routes = require('./routes');
 
 
 
-const port = 5000;
-app.listen(port, () => console.log('Server is listening on port', port));
+  app.use(routes);
+
+module.exports = app;
