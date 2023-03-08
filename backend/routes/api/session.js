78189996baa4d362a6f85cdd80df4497c6257bf4
Aaron Zamanzadeh <Zaman.aaron2@gmail.com>
2023-03-08 15:49:24 -0800

    thigns

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -27,5 +27,13 @@ const router = express.Router();
             });
         }
     );
+    // Log out
+    router.delete(
+        '/',
+        (_req, res) => {
+        res.clearCookie('token');
+        return res.json({ message: 'success' });
+        }
+    );
 
 module.exports = router;
