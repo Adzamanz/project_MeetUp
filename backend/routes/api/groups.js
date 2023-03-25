947e0203019c5bf0f124aa923b6034b5a02a45ff
Aaron Zamanzadeh <Zaman.aaron2@gmail.com>
2023-03-25 09:06:40 -0700

    fixed create group validation bug

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -42,7 +42,7 @@ router.get(
 
         let newGroups = await Group.findAll({raw: true});
 
-        res.json({group: await Promise.all(newGroups.map((data) => addContextToGroup(data)))});
+        res.json({Groups: await Promise.all(newGroups.map((data) => addContextToGroup(data)))});
 
     })
 //create new group
