9b176115b7451f9c2b2551ea53c3e11a8c55fbe2
Aaron Zamanzadeh <Zaman.aaron2@gmail.com>
2023-04-29 22:38:45 -0700

    started greet page

---
+++ frontend/src/components/Greeting/index.js
@@ -0,0 +1,31 @@
+import './Greeting.css';
+const Greeting = () => {
+    return(
+        <div id="greeting-page">
+            <div id="a">
+                    The people platform- Where interests become friendships.
+                </div>
+            <div id="b">
+                <img src="" alt=''/>
+            </div>
+            <div id="c">
+                How Meetup works
+            </div>
+            <div id="d">
+                <div id="da">
+                    See All Groups
+                </div>
+                <div id="db">
+                    See All Events
+                </div>
+                <div id="dc">
+                    Start a new group
+                </div>
+            </div>
+            <div id="e">
+                "Join Meetup" button here
+            </div>
+        </div>
+    )
+}
+export default Greeting;
