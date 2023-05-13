5bb3122205552b8aee399d4813a0db6c808523c3
Aaron Zamanzadeh <Zaman.aaron2@gmail.com>
2023-05-13 09:38:07 -0700

    node module edit 1

--- frontend/src/store/csrf.js
+++ frontend/src/store/csrf.js
@@ -1,3 +1,4 @@
+import Cookies from 'js-cookie';
 export async function csrfFetch(url, options = {}) {
   // set options.method to 'GET' if there is no method
   options.method = options.method || 'GET';
