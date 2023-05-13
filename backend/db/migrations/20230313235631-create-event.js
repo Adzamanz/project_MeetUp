a261af4ba288468c89d3aa7125f2b20063116cb4
Aaron Zamanzadeh <Zaman.aaron2@gmail.com>
2023-05-13 10:51:01 -0700

    omg two of them

--- backend/db/migrations/20230313235631-create-event.js
+++ backend/db/migrations/20230313235631-create-event.js
@@ -30,7 +30,7 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       type: {
-        type: Sequelize.ENUM('Online','In person')
+        type: Sequelize.ENUM('Online','In Person')
       },
       price: {
         type: Sequelize.FLOAT
